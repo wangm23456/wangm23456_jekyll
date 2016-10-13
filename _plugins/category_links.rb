551fe1120dba87c8b9fe81b5b930c4553434efbc
wangm23456 <wangm23456@163.com>
2016-10-13 15:24:46 +0800

    archive

--- _plugins/category_links.rb
+++ _plugins/category_links.rb
@@ -1,14 +1,23 @@
 module Jekyll
 	module CategoryLinksFilter
 		def category_links(categories)
+			jekyll_archives_installed = Jekyll.const_defined?('Archives', false)
 			return '' if categories.empty?
+
 			output = []
 			categories.each do |cat|
-				output << cat
+				output << (jekyll_archives_installed ? category_link(cat, @context) : cat)
 			end
 			configs = @context.registers[:site].config
 			seperator = configs['categories_seperator'] || ', '
 			output.join(seperator)
+		end
+
+		def category_link(category, context)
+			site = context.registers[:site]
+			archive = Archives::Archive.new(site, category, 'category', [])
+			url = site.config['baseurl'] + archive.url
+			%(<a href="#{url}" title="#{category}">#{category}</a>)
 		end
 	end
 end
